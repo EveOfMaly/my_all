574415b789cfdbc6fcc8fc95bf37b7fd50dc7d8a
EveOfMaly <john.a.kyeremeh@gmail.com>
2021-09-02 13:50:26 -0700

    completed assignment

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,7 @@
 require 'pry'
 
+
+#build the all? method 
 def my_all?(collection)
     i = 0
     block_return_value = []
@@ -15,3 +17,56 @@ def my_all?(collection)
     end
 end
 
+
+
+#build the none? method 
+
+collection = [2,3]
+def none?(collection)
+    i = 0 
+    none = []
+    while i < collection.length
+        none << yield(collection[i])
+        i += 1
+  
+    end
+    
+    if none.include?(true)
+        false
+    else
+        true
+    end
+end
+
+
+#build the any? method 
+
+def any?(collection)
+    i = 0 
+    any = []
+    while i < collection.length
+        any << yield(collection[i])
+        i += 1
+  
+    end
+    
+    if any.include?(true)
+        true
+    else
+        false
+    end
+end
+
+
+#building the find/detech method 
+#will return all elements from the original collection that cause the block
+# to evaluate to true, #detect will only return the first element that makes the block true.
+
+def my_find(collection)
+    i = 0 
+    find = []
+    while i < collection.length
+        return collection[i] if yield(collection[i])
+        i += 1
+    end
+end
